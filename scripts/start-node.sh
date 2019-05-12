0c9d107234d9614979981ffc245e3eb43c907d14
Johnny Lin <hijohnnylin@gmail.com>
2019-05-12 02:30:16 -0700

    Update PM2 on deploy

--- scripts/start-node.sh
+++ scripts/start-node.sh
@@ -3,6 +3,7 @@
 export NODE_ENV=production
 
 cd /home/node/main
+pm2 updatePM2 # pick up new node version if we upgraded
 pm2 delete all
 pm2 start index.js -i max --merge-logs --log ../logs/app.log --node-args="--icu-data-dir=node_modules/full-icu"
 pm2 save
